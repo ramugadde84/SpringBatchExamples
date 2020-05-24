26aff9ffd21edf7ff41862fadc1269366d93b2ed
Ramu <ramugadde84@gmail.com>
2020-05-24 14:28:46 +0530

    Spring Batch Example starting

---
+++ src/test/java/com/example/SpringBatchExamples/SpringBatchExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchExamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
